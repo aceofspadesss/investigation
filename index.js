9a2c06ca525ce598686f4e787c2461d558911813
aceofspadesss <61066869+aceofspadesss@users.noreply.github.com>
2022-06-21 15:40:55 +0300

    commented out geolocation api, added redirect

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ let dataHolder = document.querySelector('#data-holder');
 let data = document.querySelector('#data');
 let params = new URLSearchParams(window.location.search);
 let token = params.get('token');
-let decoded_token = atob(token).split('_');
-let phone_number = decoded_token[0];
-let invite_uid = decoded_token[1];
-
-
 
 let formed_json = {
   userAgent: window.navigator.userAgent,
@@ -14,23 +9,14 @@ let formed_json = {
   screenHeight: window.screen.height,
   colorDepth: window.screen.colorDepth,
   deviceCPUCores: navigator.hardwareConcurrency,
-  token: params.get('token'),
-  phone_number: phone_number,
-  invite_uid: invite_uid
 }
 
-const accessAllowed = (callback) => {
-  formed_json.geo_latitude = callback.coords.latitude;
-  formed_json.geo_longitude = callback.coords.longitude;
-
-  displayData(formed_json);
-};
-
-const accessDenied = (callback) => {
-  console.log(callback);
-
-  displayData(formed_json);
-};
+if (token != null) {
+  formed_json.token = token;
+  formed_json.decodedToken = atob(token).split('_');
+  formed_json.phoneNumber = decoded_token[0];
+  formed_json.inviteUID = decoded_token[1];
+}
 
 const displayData = (object) => {
   for (const [key, value] of Object.entries(object)) {
@@ -40,4 +26,28 @@ const displayData = (object) => {
   }
 }
 
-navigator.geolocation.getCurrentPosition(accessAllowed, accessDenied);
+formed_json.keyValuePair = [formed_json.userAgent, `${formed_json.screenWidth}x${formed_json.screenHeight}`, formed_json.colorDepth, formed_json.deviceCPUCores].join('|');
+
+displayData(formed_json);
+
+// Redirect to appstore
+// window.location.replace("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwjF5qf1x774AhXt_CoKHYy6BlQQFnoECBcQAQ&url=https%3A%2F%2Fapps.apple.com%2Fua%2Fapp%2Fpeer%2Fid1585616081%3Fl%3Dru&usg=AOvVaw1jO_5x_TLE1N80DEKfBruV")
+
+// GEOLOCATION API LOGIC
+//
+// const accessAllowed = (callback) => {
+//   formed_json.geo_latitude = callback.coords.latitude;
+//   formed_json.geo_longitude = callback.coords.longitude;
+
+//   displayData(formed_json);
+// };
+
+// const accessDenied = (callback) => {
+//   console.log(callback);
+
+//   displayData(formed_json);
+// };
+// GEOLOCATION API CALL
+//
+// navigator.geolocation.getCurrentPosition(accessAllowed, accessDenied);
+
